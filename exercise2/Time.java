7e70c1f3adae9c573d392400f3428376c589bfdc
1nbetw33n <a.ab@mail.de>
2021-05-13 22:50:46 +0200

    Added all files from se1-team20 repo.

---
+++ exercise2/Time.java
@@ -0,0 +1,13 @@
+package exercise2;
+
+public class Time implements TimeIF{
+    @Override
+    public int hours() {
+        return 0;
+    }
+
+    @Override
+    public int minutes() {
+        return 0;
+    }
+}
---
+++ exercise2/sort_a_string.java
@@ -0,0 +1,8 @@
+package exercise2;
+
+public interface sort_a_string {
+
+    static boolean isWellSorted(String[] sequence){
+        return true;
+    }
+}
