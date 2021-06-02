a3be49fc6f605eb5637ac6e4812c144479cbebf2
Preet149 <82959745+Preet149@users.noreply.github.com>
2021-06-02 17:46:10 +0200

    Made some changes and added comments to make the code readable.

--- src/main/parkhaus-project/Parkhaus/java/com/se1_team20/Parkhaus/Checkout.java
+++ src/main/parkhaus-project/Parkhaus/java/com/se1_team20/Parkhaus/Checkout.java
@@ -19,37 +19,58 @@
  */
 
 package com.se1_team20.Parkhaus;
-
+/*Preet: Sobald man auf ein Auto klickt, wird der counter gestoppt und eine Checkout Klasse wird instanziiert.
 
 /* TODO: sollte die klasse eigene instanzvariablen haben? */
 
 public class Checkout {
 
-double price;
-long bill_num;
-long duration;
+    private double price;    //fixed price per minutes
+    private double bill;    //Payment to be payed by the driver before checking out.
+    long bill_num;   // Ticketnumber
+    private long duration;   // Parkdauer
 
-
-
-
+    //Saves the important information for every car.
     public Checkout(double price, long bill_Number, long duration){
 
+        this.bill=price*duration;
 
+        setPrice(this.price);
 
+        this.bill_num=bill_Number;
+        this.duration=duration;
     }
 
     /* TODO: sollte die checkout methode in der lage sein, den preis zu verändern? */
     /* TODO: sollte die klasse checkout instanziierbar sein, oder eine helperklasse mit statischen methoden sein? */
-    public void setPrice() {
 
 
+    //Preet: Für Firmenkunde könnte der Preis auf 0 gesetzt werden.
+
+    //Preet: Setting the fixed price per minutes.
+    public void setPrice(double price){
+
+          this.price=price;
+    }
+
+    public double getPrice(double total_amount){
+
+        return total_amount;
+    }
+
+    //Saving the amount to be payed.
+    public void setBill(double bill){
+
+        this.bill=bill;
+    }
+
+    //Preet: Getting the bill.
+    public double getBill(){
+
+        return this.bill;
     }
 
 
-   public double getPrice(double total_amount){
-
-    return total_amount;
-   }
 
 
 
--- src/main/parkhaus-project/Parkhaus/java/com/se1_team20/Parkhaus/CheckoutServlet.java
+++ src/main/parkhaus-project/Parkhaus/java/com/se1_team20/Parkhaus/CheckoutServlet.java
@@ -28,4 +28,6 @@ public class CheckoutServlet extends HttpServlet {
 
 
 
+
+
 }
