8ec133879247a36732e2159243e636e8312430cb
1nbetw33n <a.ab@mail.de>
2021-06-03 16:23:06 +0200

    Created CashUni, Kurs and Student as Group

---
+++ src/main/parkhaus-project/Parkhaus/java/exercise7/Student.java
@@ -0,0 +1,35 @@
+/*
+ *  copyright (c) 2021 se1_team20.
+ *  Planet Earth, Milky Way, Virgo Supercluster.
+ *  All rights reserved.
+ *
+ *  part of team20:
+ *
+ *  1nbetw33n
+ *  Masternuss
+ *  Preet149
+ *  Rahgawi
+ *
+ * This software is the confidential and proprietary information of
+ * se1_team20. ("Confidential Information"). You shall not disclose
+ * such Confidential Information and shall use it only in accordance
+ * with the terms of the license agreement you entered into with
+ * se1_team20.
+ *
+ */
+
+package exercise7;
+
+public class Student {
+
+    protected boolean istBerufstätig;
+
+    public Student(boolean berufstätig , Kurs[] kurse){
+
+    }
+
+    public void addKurs(){
+
+    }
+
+}
