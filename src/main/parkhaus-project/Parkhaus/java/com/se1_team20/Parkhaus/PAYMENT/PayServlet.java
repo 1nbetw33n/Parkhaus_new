a587b466beff2251cd63427a1fd464a99d59c795
Preet149 <82959745+Preet149@users.noreply.github.com>
2021-07-01 12:33:17 +0200

    Made a PayServlet.java in the directory PAYMENT

---
+++ src/main/parkhaus-project/Parkhaus/java/com/se1_team20/Parkhaus/PAYMENT/PayServlet.java
@@ -0,0 +1,39 @@
+/*
+ *  copyright (c) 2021 se1_team20.
+ *  Planet Earth, Milky Way, Virgo Supercluster.
+ *  All rights reserved.
+ *
+ *  part of team20:
+ *
+ *  1nbetw33n
+ *  Masternuss
+ *  Preet149
+ *  Rahgawi
+ *
+ * This software is the confidential and proprietary information of
+ * se1_team20. ("Confidential Information"). You shall not disclose
+ * such Confidential Information and shall use it only in accordance
+ * with the terms of the license agreement you entered into with
+ * se1_team20.
+ *
+ */
+
+package com.se1_team20.Parkhaus.PAYMENT;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "PayServlet", value = "/PayServlet")
+public class PayServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
