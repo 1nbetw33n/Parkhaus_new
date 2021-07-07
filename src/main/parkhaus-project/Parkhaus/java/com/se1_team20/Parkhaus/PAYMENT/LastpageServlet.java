3b114cea43aca18ab0ed696e1c3e3ec027f5876e
Preet149 <82959745+Preet149@users.noreply.github.com>
2021-07-07 14:26:14 +0200

    Added 2 code of lines in the LastpageServlet

--- src/main/parkhaus-project/Parkhaus/java/com/se1_team20/Parkhaus/PAYMENT/LastpageServlet.java
+++ src/main/parkhaus-project/Parkhaus/java/com/se1_team20/Parkhaus/PAYMENT/LastpageServlet.java
@@ -27,13 +27,15 @@ import java.io.IOException;
 
 @WebServlet(name = "LastpageServlet", value = "/LastpageServlet")
 public class LastpageServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {}
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        response.setContentType("text/html");
+
+        request.getRequestDispatcher("Abschluss.jsp").forward(request,response);
     }
 }
