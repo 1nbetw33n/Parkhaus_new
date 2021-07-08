52cd8683c965e38a9772a7822267fdb8c1e3fad6
Masternuss <busse.l.95@gmail.com>
2021-07-08 13:51:45 +0200

    deleted TODOS in handleEnter

--- src/main/parkhaus-project/Parkhaus/java/com/se1_team20/Parkhaus/PARKHAUS/ParkhausServlet.java
+++ src/main/parkhaus-project/Parkhaus/java/com/se1_team20/Parkhaus/PARKHAUS/ParkhausServlet.java
@@ -136,14 +136,9 @@ public abstract class ParkhausServlet extends ParkingServlet {
 
     private void handleEnter(final String[] PARAMS)
     {
-        //TODO: Parkplätze implementieren
-
         parkingspaces.set(Integer.parseInt(PARAMS[7])-1,"enter"+","+PARAMS[1]+","+PARAMS[7]);
-
-
         CarIF newCar = new Car( PARAMS );
         cars().add( newCar );
- // TODO: IF-Abfrage ,falls Event occupied,dann kein Auto hinzufügen
     }
 
      private void handleLeave(final String[] PARAMS)
