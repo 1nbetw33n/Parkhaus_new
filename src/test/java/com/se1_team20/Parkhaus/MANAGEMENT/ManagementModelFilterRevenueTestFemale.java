c18a867626ac22c4a6a7bb0cd3c846b31c0167f2
1nbetw33n <a.ab@mail.de>
2021-07-09 17:55:03 +0200

    Created ManagementModelFilterRevenueTestFemale

---
+++ src/test/java/com/se1_team20/Parkhaus/MANAGEMENT/ManagementModelFilterRevenueTestFemale.java
@@ -0,0 +1,28 @@
+/*
+ *  copyright (c) 2021 se1_team20.
+ *  Planet Earth, Milky Way, Virgo Supercluster.
+ *  All rights reserved.
+ *
+ *  part of team20:
+ *
+ *  1nbetw33n
+ *  Masternuss
+ *  Preet149
+ *  Rahgawi
+ *
+ * This software is the confidential and proprietary information of
+ * se1_team20. ("Confidential Information"). You shall not disclose
+ * such Confidential Information and shall use it only in accordance
+ * with the terms of the license agreement you entered into with
+ * se1_team20.
+ *
+ */
+
+package com.se1_team20.Parkhaus.MANAGEMENT;
+/*
+1nbetw33n
+09.Jul.2021
+*/
+
+public class ManagementModelFilterRevenueTestFemale {
+}
