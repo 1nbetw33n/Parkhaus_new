da69310085999c01613398b85ab9c0d157038b24
Preet149 <82959745+Preet149@users.noreply.github.com>
2021-07-09 06:34:54 +0200

    Slighty changed in the FirstpageServlet

--- src/main/parkhaus-project/Parkhaus/java/com/se1_team20/Parkhaus/PAYMENT/FirstpageServlet.java
+++ src/main/parkhaus-project/Parkhaus/java/com/se1_team20/Parkhaus/PAYMENT/FirstpageServlet.java
@@ -25,7 +25,7 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "Servlet", value = "/Servlet")
+@WebServlet("/FirstpageServlet")
 public class FirstpageServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
