f01ed8f2107c22eae65dff69ecc03d1b88647a31
1nbetw33n <a.ab@mail.de>
2021-07-11 11:05:52 +0200

    Renamed file

---
+++ src/test/java/com/se1_team20/Parkhaus/MANAGEMENT/FILTER_REVENUE_TESTS/ManagementModelFilterRevenueByVehicleTestSUV.java
@@ -0,0 +1,64 @@
+/*
+ *  copyright (c) 2021 se1_team20.
+ *  Planet Earth, Milky Way, Virgo Supercluster.
+ *  All rights reserved.
+ *
+ *  part of team20:
+ *
+ *  1nbetw33n
+ *  Masternuss
+ *  Preet149
+ *  Rahgawi
+ *
+ * This software is the confidential and proprietary information of
+ * se1_team20. ("Confidential Information"). You shall not disclose
+ * such Confidential Information and shall use it only in accordance
+ * with the terms of the license agreement you entered into with
+ * se1_team20.
+ *
+ */
+
+package com.se1_team20.Parkhaus.MANAGEMENT.FILTER_REVENUE_TESTS;
+/*
+1nbetw33n
+11.Jul.2021
+*/
+
+import com.se1_team20.Parkhaus.MANAGEMENT.ManagementModelTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class ManagementModelFilterRevenueByVehicleTestSUV extends ManagementModelTest {
+
+    /* TESTS FOR HANDICAPPED */
+
+    @Test
+    @DisplayName("filterRevenue for handicapped - equals test")
+    void handicappedEqualsTestFilterRevenue()
+    {
+        assertEquals(0.0,  mModel.filterRevenueByVehicle(new ArrayList<>(), "Handicapped"));
+        assertEquals(5168.,  mModel.filterRevenueByVehicle(this.cars, "Handicapped"));
+    }
+
+    @Test
+    @DisplayName("filterRevenue for handicapped - not equals test")
+    void handicappedNotEqualsTestFilterRevenue()
+    {
+        assertNotEquals(5168., mModel.filterRevenueByVehicle(new ArrayList<>(), "Handicapped"));
+        assertNotEquals(0, mModel.filterRevenueByVehicle(this.cars, "Handicapped"));
+    }
+
+    @Test
+    @DisplayName("filterRevenue for handicapped - not null test")
+    void handicappedNotNullTestFilterRevenue()
+    {
+        assertNotNull(mModel.filterRevenueByVehicle(new ArrayList<>(), "Handicapped"));
+        assertNotNull(mModel.filterRevenueByVehicle(this.cars, "Handicapped"));
+    }
+
+}
