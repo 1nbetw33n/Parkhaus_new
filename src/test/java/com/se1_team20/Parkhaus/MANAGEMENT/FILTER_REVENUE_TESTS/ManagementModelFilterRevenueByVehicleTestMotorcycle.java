28c4e1fc3ff6bfe0572a0bf262f3300eb62a57da
1nbetw33n <a.ab@mail.de>
2021-07-11 11:43:35 +0200

    Successfully run equals test

--- src/test/java/com/se1_team20/Parkhaus/MANAGEMENT/FILTER_REVENUE_TESTS/ManagementModelFilterRevenueByVehicleTestMotorcycle.java
+++ src/test/java/com/se1_team20/Parkhaus/MANAGEMENT/FILTER_REVENUE_TESTS/ManagementModelFilterRevenueByVehicleTestMotorcycle.java
@@ -25,6 +25,23 @@ package com.se1_team20.Parkhaus.MANAGEMENT.FILTER_REVENUE_TESTS;
 */
 
 import com.se1_team20.Parkhaus.MANAGEMENT.ManagementModelTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ManagementModelFilterRevenueByVehicleTestMotorcycle extends ManagementModelTest {
+
+    //TESTS FOR MOTORCYCLE
+
+    @Test
+    @DisplayName("filterRevenue for Motorcycle - equals test")
+    void motorcycleEqualsTestFilterRevenue()
+    {
+        assertEquals(0.,  mModel.filterRevenueByVehicle(new ArrayList<>(), "Motorcycle"));
+        assertEquals(0.,  mModel.filterRevenueByVehicle(this.cars, "Motorcycle")); // expects 0., because the only motorcycle is a company customers
+    }
+
 }
